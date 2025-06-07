f03294a0785b89a1c40daf2fc9b95f0089288f7e
Grace Yun <graceyunn@gmail.com>
2025-06-07 19:43:11 -0400

    max queue

--- sliding-window-max.ts
+++ sliding-window-max.ts
@@ -1,29 +1,29 @@
 export const slidingWindowMax = (input: number[], k: number) => {
   const result: number[] = []
-  let lastMaxIdx: number | undefined
+  const candidates: number[] = [] // data structure: monotonic decreasing queue / max queue
 
   for (let i = 0; i < input.length; i++) {
-    // prev max is outside current window
-    // find new valid max within current window
-    if (lastMaxIdx !== undefined && lastMaxIdx <= i - k) {
-      let newMaxIdx = i - k + 1
+    const current = input[i]
+    const start = i - k + 1
 
-      for (let j = newMaxIdx; j <= i; j++) {
-        if (input[j]! >= input[newMaxIdx]!) {
-          newMaxIdx = j
-        }
-      }
-
-      result[i] = input[newMaxIdx]!
-      lastMaxIdx = newMaxIdx
-    } // found new max within current window
-    else if (lastMaxIdx === undefined || input[i]! >= input[lastMaxIdx]!) {
-      lastMaxIdx = i
-      result[i] = input[i]!
-    } // prev max is inside current window and still valid
-    else {
-      result[i] = input[lastMaxIdx]!
+    // remove candidates outside current window
+    while (candidates.length > 0 && candidates[0]! < start) {
+      candidates.shift()
     }
+
+    // remove candidates smaller than/equal to current value
+    while (
+      candidates.length > 0 &&
+      input[candidates[candidates.length - 1]] <= current
+    ) {
+      candidates.pop()
+    }
+
+    // add index to potential future max candidates
+    candidates.push(i)
+
+    // because we've sorted candidates in decreasing order, head of array is current window max
+    result.push(input[candidates[0]])
   }
 
   return result
