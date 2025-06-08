7f90163c3d0934c49dd7747a3bf8ea74bdaff323
Grace Yun <graceyunn@gmail.com>
2025-06-07 20:12:29 -0400

    comments + more tests

--- sliding-window-max.ts
+++ sliding-window-max.ts
@@ -6,23 +6,30 @@ export const slidingWindowMax = (input: number[], k: number) => {
     const current = input[i]
     const start = i - k + 1
 
-    // remove candidates outside current window
+    // THE SECOND TIME WE EVER TOUCH AN INDEX IN THE ARRAY IS EITHER:
+    // 1. when it's outside the current window (first while loop)
+    // 2. when it's no longer relevant to us because it's smaller than the current value (second while loop)
+
     while (candidates.length > 0 && candidates[0]! < start) {
+      // we shift (remove from head of array) when value at index is outside window
       candidates.shift()
     }
 
-    // remove candidates smaller than/equal to current value
     while (
       candidates.length > 0 &&
       input[candidates[candidates.length - 1]] <= current
     ) {
+      // we pop (remove from tail of array) when value at index is smaller than the current value (meaning it is no longer a valid candidate)
       candidates.pop()
     }
 
-    // add index to potential future max candidates
-    candidates.push(i)
+    candidates.push(i) // THE FIRST PLACE WE ACCESS AN INDEX IN THE INPUT ARRAY: when we add it to the candidates array
 
-    // because we've sorted candidates in decreasing order, head of array is current window max
+    // each index is added ONCE and removed AT MOST ONCE from our candidates array (so, each element is touched at most TWICE)
+    // rather than iterating over each K window per element, we keep a running list of max candidates (without needing to ever explicitly "sort" them) because we remove ineligible candidates as we iterate over the input array
+    // that makes this solution O(n), not O(n * k)
+
+    // because candidates are in decreasing order, head of array is current window max
     result.push(input[candidates[0]])
   }
 
